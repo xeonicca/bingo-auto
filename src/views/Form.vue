cc9f02a206b22a89e585fdd6089ce012036df563
Steve Yu <xeonicca@staff.ruten.com.tw>
2022-01-13 10:45:37 +0800

    [modify] add special number fields

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -4,13 +4,15 @@ import { airtable } from '@/api/airtable'
 export default {
   data: () => ({
     inputArray: Array(25),
+    specialArray: Array(7)
   }),
 
   methods: {
     async submit() {
       let numbers = this.inputArray.join(',')
+      let specials = this.specialArray.join(',')
       let response =  await airtable.post(`pool`, {
-        fields: {numbers} 
+        fields: {numbers, specials}
       })
 
       alert(`剛剛送出那張的編號是${response.data.fields.id}`)
@@ -19,6 +21,7 @@ export default {
 
     clearForm() {
       this.inputArray = Array(25)
+      this.specialArray = Array(7)
     }
   }
 }
@@ -32,20 +35,50 @@ export default {
         class="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200"
       >
         <div class="p-1">
+          <h2 class="pl-4 py-2 text-lg font-bold" >一般號</h2>
           <div class="overflow-x-auto">
             <table class="table-auto w-full">
               <tbody class="text-xl divide-y divide-gray-100">
                 <tr v-for="(row, i) in 5">
                   <td
                     v-for="(num, j) in 5"
-                    class="p-2 text-center"
+                    class="p-1 text-center"
                     :key="i * 5 + j"
                   >
                     <input
-                      class="w-12 p-2 text-xl text-center rounded-lg border-2 border-indigo-200"
+                      class="w-full p-2 text-xl text-center rounded-lg border-2 border-indigo-200"
                       type="tel"
                       :tabindex="i * 5 + j + 1"
                       v-model="inputArray[i * 5 + j]"
+                      maxlength="2"
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="mt-4 w-full max-w-2xl mx-auto bg-stone-100 shadow-lg rounded-sm border border-gray-200"
+      >
+        <div class="p-1">
+          <h2 class="pl-4 py-2 text-lg font-bold" >特殊號</h2>
+          <div class="overflow-x-auto">
+            <table class="table-auto w-full">
+              <tbody class="text-xl divide-y divide-gray-100">
+                <tr>
+                  <td
+                    v-for="(num, i) in 7"
+                    class="p-1 text-center"
+                    :key="26 + i"
+                  >
+                    <input
+                      class="w-full p-2 text-xl text-center rounded-lg border-2 border-indigo-200"
+                      type="tel"
+                      :tabindex="26 + i"
+                      v-model="specialArray[i]"
                       maxlength="2"
                     />
                   </td>
